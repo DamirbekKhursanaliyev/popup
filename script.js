ed88187231db31eb90fb10b140ab0a2d5055e0ef
Damirbek Xursanaliyev <110587050+DamirbekKhursanaliyev@users.noreply.github.com>
2022-08-04 11:30:37 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+let poppup = document.getElementById("poppup");
+
+function openPoppup(){
+    poppup.classList.add("open-poppup");
+}
+function closePoppup(){
+    poppup.classList.remove("open-poppup")
+}
